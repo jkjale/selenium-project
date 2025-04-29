67026241ecb714ca307e49d2efb8491e0f92800d
Jake Lee <jkjalee@gmail.com>
2025-04-28 20:49:55 -0700

    Complete script for QAAR-875

--- features/environment.py
+++ features/environment.py
@@ -17,9 +17,9 @@ def browser_init(context, scenario_name):
     # context.driver = webdriver.Chrome(service=service)
 
     ### FIREFOX ###
-    driver_path = GeckoDriverManager().install()
-    service = Service(driver_path)
-    context.driver = webdriver.Firefox(service=service)
+    # driver_path = GeckoDriverManager().install()
+    # service = Service(driver_path)
+    # context.driver = webdriver.Firefox(service=service)
 
     ### HEADLESS MODE ####
     # options = webdriver.ChromeOptions()
@@ -33,18 +33,18 @@ def browser_init(context, scenario_name):
     ### BROWSERSTACK ###
     # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
     # To change devices go to https://www.browserstack.com/docs/automate/capabilities and go to "Legacy" and choose device
-    # bs_user = '***'
-    # bs_key = '***'
-    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-    # options = Options()
-    # bstack_options = {
-    #     "os": "Windows",
-    #     "osVersion": "11",
-    #     'browserName': 'chrome',
-    #     'sessionName': scenario_name,
-    # }
-    # options.set_capability('bstack:options', bstack_options)
-    # context.driver = webdriver.Remote(command_executor=url, options=options)
+    bs_user = 'jakelee_JI4Kfw'
+    bs_key = 'Tj9xcB5kA3YHSJMzETfq'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    options = Options()
+    bstack_options = {
+        "os": "Windows",
+        "osVersion": "11",
+        'browserName': 'chrome',
+        'sessionName': scenario_name,
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
